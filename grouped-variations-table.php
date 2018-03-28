73a3dde36db5ecae09fb111a32b7774707b7d2bb
Mattias Nording <mattiasnording@gmail.com>
2018-03-28 05:14:47 +0200

    Initial commit

---
+++ grouped-variations-table.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:  Grouped Variations Table
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Allowing you to group variations in sleak tables on the product page
+Version:      0.0.1
+Author:       mnording
+Author URI:   https://mnording.com/
+License:      MIT
+License URI:  https://opensource.org/licenses/MIT
+Text Domain:  groupedvartable
+Domain Path:  /languages
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
---
+++ readme.txt
@@ -0,0 +1,7 @@
+=== Grouped Variations Table ===
+Contributors: mnording
+Tags: variations, woocommerce, table
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+
+Allowing you to group variations in sleak tables on the product page
